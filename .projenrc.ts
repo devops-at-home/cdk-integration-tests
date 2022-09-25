13bed4891eb94fbf63bf7785d13f014bbc11a284
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2022-09-25 23:57:11 +0800

    configure projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,29 @@
-import { awscdk } from "projen";
+import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
-  authorName: "DevOps@Home",
-  authorUrl: "https://devops-at-ho.me",
-  cdkVersion: "2.43.1",
-  defaultReleaseBranch: "main",
+  authorName: 'DevOps@Home',
+  authorUrl: 'https://devops-at-ho.me',
+  cdkVersion: '2.43.1',
+  defaultReleaseBranch: 'main',
   eslint: false,
-  name: "cdk-integration-tests",
+  name: 'cdk-integration-tests',
   projenrcTs: true,
-
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 120,
+      tabWidth: 4,
+      singleQuote: true,
+    },
+  },
+  githubOptions: {
+    pullRequestLint: false,
+  },
+  license: 'MIT',
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: [
+    'cdk-iot-core-certificates',
+  ] /* Build dependencies for this module. */,
   // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
