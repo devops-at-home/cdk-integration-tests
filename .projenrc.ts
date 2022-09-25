7b4a97a49ade60298e19350c1aac1c257f0d4e74
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2022-09-26 00:24:13 +0800

    more projen config

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,29 +1,30 @@
 import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
-  authorName: 'DevOps@Home',
-  authorUrl: 'https://devops-at-ho.me',
-  cdkVersion: '2.43.1',
-  defaultReleaseBranch: 'main',
-  eslint: false,
-  name: 'cdk-integration-tests',
-  projenrcTs: true,
-  prettier: true,
-  prettierOptions: {
-    settings: {
-      printWidth: 120,
-      tabWidth: 4,
-      singleQuote: true,
+    authorName: 'DevOps@Home',
+    authorUrl: 'https://devops-at-ho.me',
+    cdkVersion: '2.43.1',
+    defaultReleaseBranch: 'main',
+    eslint: false,
+    name: 'cdk-integration-tests',
+    projenrcTs: true,
+    prettier: true,
+    prettierOptions: {
+        settings: {
+            printWidth: 120,
+            tabWidth: 4,
+            singleQuote: true,
+        },
     },
-  },
-  githubOptions: {
-    pullRequestLint: false,
-  },
-  license: 'MIT',
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  devDeps: [
-    'cdk-iot-core-certificates',
-  ] /* Build dependencies for this module. */,
-  // packageName: undefined,  /* The "name" in package.json. */
+    githubOptions: {
+        pullRequestLint: false,
+    },
+    license: 'MIT',
+    // deps: [],                /* Runtime dependencies of this module. */
+    // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+    devDeps: ['cdk-iot-core-certificates'] /* Build dependencies for this module. */,
+    // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.jest!.addTestMatch('**/?(*.)@(spec|test).[tj]s?(x)');
+
 project.synth();
