b06403db3a6acd8b0307095d1feab63e889687f5
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2022-09-26 22:17:04 +0800

    rename file

--- src/stack-factory.ts
+++ src/stack-factory.ts
@@ -1,5 +1,5 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
-import { IotCoreCertificates } from './stacks/iot-core-certificates-stack';
+import { IotCoreCertificates } from './stacks/iot-core-certificates.stack';
 
 export class StackFactory {
     public readonly stacks: Stack[] = [];
