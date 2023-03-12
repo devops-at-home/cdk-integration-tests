11ee29f53c79393852aeeb74d2d97b4f7aff9979
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2023-03-12 18:46:57 +0800

    update deps

--- src/stacks/iot-core-certificates.stack.ts
+++ src/stacks/iot-core-certificates.stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ThingWithCert } from 'cdk-iot-core-certificates';
 
@@ -6,19 +6,26 @@ export class IotCoreCertificates extends Stack {
     constructor(scope: Construct, id: string, props: StackProps = {}) {
         super(scope, id, props);
 
-        // const { certId, thingArn } = new ThingWithCert(this, 'ThingWithCert', {
-        new ThingWithCert(this, 'ThingWithCert', {
+        const { thingArn, certId, certPem, privKey } = new ThingWithCert(this, 'ThingWithCert', {
             thingName: 'integrationTest',
             saveToParamStore: true,
             paramPrefix: 'devices',
         });
 
-        // new CfnOutput(this, 'Output-CertId', {
-        //     value: certId,
-        // });
+        new CfnOutput(this, 'Output-ThingArn', {
+            value: thingArn,
+        });
 
-        // new CfnOutput(this, 'Output-ThingArn', {
-        //     value: thingArn,
-        // });
+        new CfnOutput(this, 'Output-CertId', {
+            value: certId,
+        });
+
+        new CfnOutput(this, 'Output-CertPem', {
+            value: certPem,
+        });
+
+        new CfnOutput(this, 'Output-PrivKey', {
+            value: privKey,
+        });
     }
 }
